ffd611671c0b56555e77a6eabf75009e34043499
zbay <zlwilliams1@gmail.com>
2018-08-18 16:58:41 -0400

    Map checkpoint: setting corners based on max/min latitude and longitude

--- public/src/app/modules/map/components/map/map.component.ts
+++ public/src/app/modules/map/components/map/map.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 
-import { Icon, latLng, marker, tileLayer } from 'leaflet';
+import { Icon, latLng, latLngBounds, LatLngBounds, marker, tileLayer } from 'leaflet';
 import { DestinationResult } from '@models/types';
 import { DestinationType } from '@models/enums';
 
@@ -29,14 +29,13 @@ const emptyIcon = new MarkerIcon({iconUrl: '../../assets/map_marker-red.png'});
 export class MapComponent implements OnInit {
   @Input() searchResults: DestinationResult[] = [];
 
+  fitBounds: LatLngBounds;
+  layers = [];
   options = {
     layers: [
       tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
-    ],
-    zoom: 10,
-    center: latLng(46.879966, -121.726909)
+    ]
   };
-  layers = [];
 
   constructor() {}
 
@@ -56,20 +55,40 @@ export class MapComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.centerMap();
+    // this.centerMap();
     this.searchResults.map((result) => {
       this.layers.push(marker([result.coordinates.latitude, result.coordinates.longitude],
         {title: result.name, icon: MapComponent.getIcon(result.category) }));
     });
+    this.setMapBounds();
   }
 
-  centerMap() {
-    const centerCoordinates = this.searchResults
-      .reduce((sumOfCoordinates, searchResults) => {
-        return [sumOfCoordinates[0] + (searchResults.coordinates.latitude / this.searchResults.length),
-          sumOfCoordinates[1] + (searchResults.coordinates.longitude / this.searchResults.length)];
-      }, [0, 0]);
-    this.options.center = latLng(centerCoordinates[0], centerCoordinates[1]);
+  // centerMap() {
+  //   const centerCoordinates = this.searchResults
+  //     .reduce((sumOfCoordinates, searchResults) => {
+  //       return [sumOfCoordinates[0] + (searchResults.coordinates.latitude / this.searchResults.length),
+  //         sumOfCoordinates[1] + (searchResults.coordinates.longitude / this.searchResults.length)];
+  //     }, [0, 0]);
+  //   this.options.center = latLng(centerCoordinates[0], centerCoordinates[1]);
+  // }
+
+  setMapBounds(): void {
+    const latitudes = this.getLatitudes();
+    const longitudes = this.getLongitudes();
+    const minLat = Math.min(...latitudes) - 0.0001;
+    const maxLat = Math.max(...latitudes) + 0.05;
+    const minLong = Math.min(...longitudes) - 0.0001;
+    const maxLong = Math.max(...longitudes) + 0.0001;
+    const corner1 = latLng(minLat, minLong),
+      corner2 = latLng(maxLat, maxLong);
+    this.fitBounds = latLngBounds(corner1, corner2);
+  }
+
+  private getLatitudes() {
+    return this.searchResults.map((d) => d.coordinates.latitude);
+  }
+  private getLongitudes() {
+    return this.searchResults.map((d) => d.coordinates.longitude);
   }
 
 }
